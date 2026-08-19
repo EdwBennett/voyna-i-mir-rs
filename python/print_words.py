d256ee9cf5d0f43760faf034a2bd6059f8674319
Ed Bennett <edw.bennett@gmail.com>
2026-08-19 16:31:58 -0500

    ln from python to excerpts yaml

---
+++ python/print_words.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import yaml
+
+filename = "Volume_1_Part_1.yaml"
+
+with open(filename, "r", encoding="utf-8") as f:
+    items = yaml.safe_load(f)
+
+for item in items:
+    print()
+    print(item["chapter"])
+    print()
+    print(item["words"])
+    
