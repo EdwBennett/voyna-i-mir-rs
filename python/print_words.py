3562b8d107ba0f97912dc2aee62ae21252e94cc4
Ed Bennett <edw.bennett@gmail.com>
2026-08-19 17:05:04 -0500

    a python file to collect yaml values

--- python/print_words.py
+++ python/print_words.py
@@ -2,12 +2,14 @@
 import yaml
 
 filename = "Volume_1_Part_1.yaml"
+output_filename = "Volume_1_Part_1.txt"
 
 with open(filename, "r", encoding="utf-8") as f:
     items = yaml.safe_load(f)
 
-for item in items:
-    print()
-    print(item["chapter"])
-    print(item["words"])
-     
+with open(output_filename, "w", encoding="utf-8") as out:
+    for item in items:
+        print(file=out)
+        print(item["chapter"], file=out)
+        print(item["words"], file=out)
+        
