65a6d5ce1e9fdfa315a6cbf2446f664868fb4541
Ed Bennett <edw.bennett@gmail.com>
2026-08-29 03:59:06 -0500

    1st try to change print_words.py to yaml output

--- python/print_words.py
+++ python/print_words.py
@@ -1,13 +1,27 @@
 import yaml
 
 filename = "Volume_1_Part_1.yaml"
-output_filename = "Volume_1_Part_1.txt"
+output_filename = "output.yaml"
 
 with open(filename, "r", encoding="utf-8") as f:
     items = yaml.safe_load(f)
 
+output = [
+    {
+        "chapter": item["chapter"],
+        "id": item["id"],
+        "ru": item["ru"],
+        "en": item["en"],
+    }
+    for item in items
+]
+
 with open(output_filename, "w", encoding="utf-8") as out:
-    for item in items:
-        print(file=out)
-        print(item["chapter"], file=out)
-        print(item["words"], file=out)
+    yaml.dump(
+        output,
+        out,
+        allow_unicode=True,
+        sort_keys=False,
+        width=float("inf"),
+        default_style='"',
+    )
